59f7d56ba9613bc156c145ed4b33d3b703024b84
Nirajan Chapagain <nirajanchapagain@Nirajans-MacBook-Pro.local>
2019-09-06 20:25:24 +0545

    friday night done

--- lib/wigets/task_title.dart
+++ lib/wigets/task_title.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('My new task'),
-      trailing: Checkbox(value: false, onChanged: null),
+        title: Text(
+          'My new task',
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: TaskCheckbox(isChecked, (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        }
+        ));
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function checkbox;
+
+  TaskCheckbox(this.checkboxState,this.checkbox);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      onChanged: checkbox,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
